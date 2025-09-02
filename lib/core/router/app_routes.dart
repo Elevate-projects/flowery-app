bf20c23747b18e88d088b34297584adfd82bb5a5
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-09-02 17:27:05 +0300

    feat(FAT1-12): solving merge conflicts

--- lib/core/router/app_routes.dart
+++ lib/core/router/app_routes.dart
@@ -1,9 +1,9 @@
 import 'package:flowery_app/core/router/route_names.dart';
 import 'package:flowery_app/presentation/auth/login/views/login_view.dart';
-import 'package:flowery_app/presentation/product_details/views/product_details_view.dart';
 import 'package:flowery_app/presentation/auth/register/views/register_view.dart';
 import 'package:flowery_app/presentation/flowery_bottom_navigation/flowery_bottom_navigation.dart';
 import 'package:flowery_app/presentation/flowery_bottom_navigation/view_model/flowery_bottom_navigation_cubit.dart';
+import 'package:flowery_app/presentation/product_details/views/product_details_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
