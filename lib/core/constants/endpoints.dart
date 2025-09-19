52d7753666100e6678ff66f7a2c7609f344bbd2b
omarelsadanyy <omarelsadany600@gmail.com>
2025-09-19 12:48:12 +0300

    feat: Add additional API endpoints for cart and address management

--- lib/core/constants/endpoints.dart
+++ lib/core/constants/endpoints.dart
@@ -8,4 +8,14 @@ abstract class Endpoints {
   static const String categories = '/api/v1/categories';
   static const String products = '/api/v1/products';
   static const String logout = '/api/v1/auth/logout';
+  static const String profileResetPassword = '/api/v1/auth/change-password';
+   static const String addProductToCart = '/api/v1/cart';
+  static const String resendCode = '/api/v1/auth/forgotPassword';
+  static const String verification = '/api/v1/auth/verifyResetCode';
+  static const String resetPassword = '/api/v1/auth/resetPassword';
+  static const String removeAddress = '/api/v1/addresses/{addressId}';
+  static const String getLoggedUserCart = '/api/v1/cart';
+  static const String updateCartQuantity = '/api/v1/cart/{productId}';
+  static const String deleteCartQuantity = '/api/v1/cart/{productId}';
+  static const String addAddress = '/api/v1/addresses';
 }
